8d5b1a6c15c010e139a906c9744942d750fc3197
wuyanbin <beijingwyb@163.com>
2019-06-10 22:48:04 +0800

    游戏界面

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,24 @@
 import sys
 import pygame
+from setting import Settings
+from ship import Ship
 
-def run_game(){
-    pygame.init();
-    screen = pygame.display.set_mode((1200,800))
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # 每次循环时都重绘屏幕
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        # 让最近绘制的屏幕可见
         pygame.display.flip()
-}
 
 run_game()
---
+++ setting.py
@@ -0,0 +1,8 @@
+class Settings():
+    """存储游戏内的所有设置"""
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
